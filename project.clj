66cf83f39b796717c69bd017b7f91513e313ba77
Hong Jiang <hong.jiang@onycloud.com>
2011-08-05 13:32:48 +0800

    Add lein-clojars.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject control "0.2.0-SNAPSHOT"
+(defproject org.clojars.onycloud/control "0.2.0-SNAPSHOT"
   :description "A clojure DSL for system admin and deployment with many remote machines"
   :url "https://github.com/onycloud/clojure-control"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[lein-exec "0.1"]])
+  :dev-dependencies [[lein-clojars "0.6.0"]
+                     [lein-exec "0.1"]])
