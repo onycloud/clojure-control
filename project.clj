a403fad6d8a89b87fa6e01a8e25f7f6185320d24
Hong Jiang <hong.jiang@onycloud.com>
2011-08-05 13:13:46 +0800

    Change version to 0.2.0-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
-(defproject control "0.2.0"
+(defproject control "0.2.0-SNAPSHOT"
   :description "A clojure DSL for system admin and deployment with many remote machines"
-  :url "https://github.com/killme2008/clojure-control"
-  :author "dennis zhuang(killme2008@gmail.com)"
+  :url "https://github.com/onycloud/clojure-control"
   :dependencies [[org.clojure/clojure "1.2.0"]
-				 [org.clojure/clojure-contrib "1.2.0"]]
+                 [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[lein-exec "0.1"]])
